8a59a98df987086f1eebeebf4a2f7f41e7129690
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-13 01:08:01 +0530

    refactor(input): change the interface to show the input box

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import { useState } from 'react';
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+const CreateArea = () => {
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    });
+
+    const addItem = (e) => {
+        e.preventDefault();
+        console.log(note);
+    }
+
+
+    const handleChange = (e) => {
+        setNote((prev) => {
+            return {
+                ...prev, 
+               [ e.target.name]: e.target.value
+            }
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <input
+                    name="title"
+                    placeholder="Title"
+                    value={note.title}
+                    onChange={handleChange}
+                />
+                <textarea
+                    name="content"
+                    placeholder="Take a note..."
+                    rows="3"
+                    value={note.content}
+                    onChange={handleChange}
+                />
+                <button onClick={addItem}>Add</button>
+            </form>
+        </div>
+    )
 }
 
 export default CreateArea;
